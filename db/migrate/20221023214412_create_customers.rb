42f6087dfa9c7088405ea7fde88c6a09782b7895
higorvoltz <higor.amorim@voltzmotors.com>
2022-10-29 10:31:56 -0300

    initial

---
+++ db/migrate/20221023214412_create_customers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :email
+      t.date :birthday
+      t.text :obs
+
+      t.timestamps
+    end
+  end
+end
